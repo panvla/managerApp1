3459530c9a0cfd122eb78b79a9a25d7d79f718b7
panvla <pandvlado@gmail.com>
2023-02-06 14:20:42 +0100

    added CrossOrigin annotation.

--- managerApp1B/src/main/java/com/vladimirpandurov/managerApp1/controller/EmployeeController.java
+++ managerApp1B/src/main/java/com/vladimirpandurov/managerApp1/controller/EmployeeController.java
@@ -12,6 +12,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/users")
 @RequiredArgsConstructor
+@CrossOrigin
 public class EmployeeController {
 
     private final EmployeeService employeeService;
